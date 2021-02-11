8c5046b819c34fc5d522a0bb07d2fbcf675ba874
Jsto19 <stoney19llc@outlook.com>
2021-02-10 23:52:34 -0600

    first

---
+++ wk2hw3.py
@@ -0,0 +1,24 @@
+import math
+
+def h_area():
+    input('The width of your home: ')
+    input('The length of your home: ')
+    return h_area
+
+
+
+height = int
+width = int
+h_area = (height * width)
+
+def circle():
+    input('What is the radius of your circle? : ')
+
+    if circle == int:
+        print("Your circle's circumference is: ")
+    else:
+        print("I'm sorry. That is not correct!")
+
+circle = 2*(pi)*r^2
+pi = 3.14
+r = radius
